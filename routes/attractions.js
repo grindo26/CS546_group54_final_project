a386f1bae41577a65584384adabddd53a5b34fb1
Parth Sangle <pratiksangle6@gmail.com>
2022-12-17 05:57:08 -0500

    Modified helper functions and error messages to be more streamlined

--- routes/attractions.js
+++ routes/attractions.js
@@ -5,27 +5,16 @@ const attractionData = data.attractionsData;
 const helperFunc = require("../helpers");
 const { ObjectId } = require("mongodb");
 
-router.route("/").get(async (req, res) => {
+router.route("/:attractionId").get(async (req, res) => {
+    id = req.params.attractionId;
     try {
-        return res.status(200).json({ Hello: "There" });
+        const attractionFound = await attractionData.getAttractionById(id);
+        return res
+            .status(200)
+            .render("attractionDetails", { title: "Attraction Details", singleAttraction: attractionFound, userName: req.session.userName });
     } catch (e) {
-        return res.status(500).json("Couldn't get the attraction!");
+        return res.status(404).json(e);
     }
 });
-
-router
-.route('/:attractionId')
-.get(async (req, res) => {
-    id = req.params.attractionId;
-  try {
-    //if(!ObjectId.isValid(id)) return res.status(400).json("not a valid city")
-    const attractionFound = await attractionData.getAttractionById(id)
-    //if(!attractionFound) return res.status(404).json("not a valid attractionId")
-    return res.status(200).render('attractionDetails',  {title: "Attraction Details", singleAttraction: attractionFound});
-} catch (e) {
-    return res.status(404).json(e);
-}
-});
-
 
 module.exports = router;
