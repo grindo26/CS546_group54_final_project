22b3fd802ce3716c1acf02f5edf41835267ad604
Syed Shoaib Sahah <shoaibshah2400@gmail.com>
2022-12-18 18:32:43 -0500

    edited search and city page

--- routes/search.js
+++ routes/search.js
@@ -12,20 +12,22 @@ router
         try {
             return res.status(200).render("searchPage", { title: "Search Page" });
         } catch (e) {
-            console.log(e, "e");
             return res.status(500).json("Somethings Wrong");
         }
     })
     .post(async (req, res) => {
         let searchString = req.body.searchInput;
         try {
-            searchString = await helperFunc.execValdnAndTrim(searchString, "searchbar");
-            await helperFunc.isNameValid(searchString, "searchbar");
+            searchString = await helperFunc.execValdnAndTrim(searchString, "Search String");
+            await helperFunc.isNameValid(searchString, "Search String");
             const searchForCity = await searchingData.searchCity(searchString);
             const searchForAttraction = await searchingData.searchAttraction(searchString);
+            if(searchForCity.length == 0 && searchForAttraction.length == 0) {
+                throw { statusCode: 400, message: `Couldn't find the City or Attraction you are looking for`}
+            }
             return res.status(200).render("searchPage", { list1: searchForCity, list2: searchForAttraction });
         } catch (e) {
-            return res.status(500).render("searchPage", { error: e });
+            return res.status(e.statusCode).render("searchPage", { title : "Error" , error: e.message });
         }
     });
 module.exports = router;
